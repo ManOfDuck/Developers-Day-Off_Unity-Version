d82d250c8573a2bbfb8d2172137e84d0ead5471b
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2023-11-17 04:00:29 -0700

    idk it added a million things

--- Assets/Scripts/Simulated Editor/SimulatedScript.cs
+++ Assets/Scripts/Simulated Editor/SimulatedScript.cs
@@ -5,5 +5,24 @@ using UnityEngine.UIElements;
 
 public class SimulatedScript : MonoBehaviour
 {
-    public VisualScript visualScript;
+    public string title;
+    public string description;
+    public UIDocument scriptUI;
+
+    protected void lightUpLine(string line)
+    {
+        //Get the visual element
+        //StartCoroutine(lightCoroutine(visualElement))
+    }
+
+    private IEnumerator lightCoroutine(VisualElement line)
+    {
+        float alpha = 255;
+        float time = 0;
+        //get animation curve
+        //while animation curve has value
+        // alpha = animationCurve.eval(time)
+        // time += deltaTime
+        yield return null;
+    }
 }
