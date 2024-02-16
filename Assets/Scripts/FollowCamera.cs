14bf8d84969cd7f79f8cc61da90bdcaa21f9dc56
Ian Warren <103790584+Duckkk819@users.noreply.github.com>
2024-02-16 15:17:51 -0700

    Implemented Camera Bounds Object and Camera Prefab

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -10,20 +10,25 @@ public class FollowCamera : MonoBehaviour
     [Tooltip("Offset from the player we'll follow at (based on the camera's position and the player's position)")]
     [SerializeField] private Vector3 offset;
 
-    [SerializeField] private float leftBound;
-    [SerializeField] private float rightBound;
-    [SerializeField] private float upperBound;
-    [SerializeField] private float lowerBound;
+    [SerializeField] private BoxCollider2D cameraBoundingBox;
+    private float leftBound;
+    private float rightBound;
+    private float lowerBound;
+    private float upperBound;
+    Vector2 cameraBounds;
 
     public float shift;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (PlayerSpawn.Player == null)
-        {
-            Debug.LogError("You didn't assign something to follow!");
-        }
+        leftBound = cameraBoundingBox.bounds.min.x;
+        rightBound = cameraBoundingBox.bounds.max.x;
+        lowerBound = cameraBoundingBox.bounds.min.y;
+        upperBound = cameraBoundingBox.bounds.max.y;
+
+        cameraBounds = new Vector2(controlledCamera.aspect * controlledCamera.orthographicSize, controlledCamera.orthographicSize);
+
         transform.position = new Vector3(PlayerSpawn.Player.transform.position.x + offset.x,
         PlayerSpawn.Player.transform.position.y + offset.y, transform.position.z);
     }
@@ -31,8 +36,8 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float boundedX = Mathf.Clamp(PlayerSpawn.Player.transform.position.x + offset.x, leftBound, rightBound);
-        float boundedY = Mathf.Clamp(PlayerSpawn.Player.transform.position.y + offset.y, lowerBound, upperBound);
+        float boundedX = Mathf.Clamp(PlayerSpawn.Player.transform.position.x + offset.x, leftBound + cameraBounds.x, rightBound - cameraBounds.x);
+        float boundedY = Mathf.Clamp(PlayerSpawn.Player.transform.position.y + offset.y, lowerBound + cameraBounds.y, upperBound - cameraBounds.y);
         transform.position = new Vector3(boundedX + shift, boundedY, transform.position.z);
     }
 
