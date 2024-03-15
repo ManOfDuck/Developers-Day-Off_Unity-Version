6d750472f56de3d50ace937f932d6aba9263be1e
Ian Warren <103790584+ManOfDuck@users.noreply.github.com>
2024-03-14 20:32:30 -0600

    Created methods for adding components

--- Assets/Scripts/Simulated Editor/SimulatedObject.cs
+++ Assets/Scripts/Simulated Editor/SimulatedObject.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using static UnityEngine.UI.Image;
 
 public class SimulatedObject : MonoBehaviour
 {
@@ -34,10 +36,49 @@ public class SimulatedObject : MonoBehaviour
         }
     }
 
-
     public void OnMouseDown()
     {
         if (interactable)
             inspectorController.DisplayObject(this, defaultSprite, sprite1);
+    }
+
+    public void AddSimulatedComponent(SimulatedComponent simulatedComponent)
+    {
+        SimulatedComponent newSimulatedComponent = new()
+        {
+            parentObject = this,
+            realComponent = CopyComponent(simulatedComponent.realComponent),
+            visualComponent = simulatedComponent.visualComponent
+        };
+
+        components.Add(newSimulatedComponent);
+    }
+
+    public void AddSimulatedScript(SimulatedScript simulatedScript)
+    {
+        SimulatedScript newSimulatedScript = (SimulatedScript) CopyComponent(simulatedScript);
+        scripts.Add(newSimulatedScript);
+    }
+
+    private Component CopyComponent(Component component)
+    {
+        // Get the component's type and fields
+        System.Type componentType = component.GetType();
+        System.Reflection.FieldInfo[] componentFields = componentType.GetFields();
+
+        // Add a new component of that type to this object
+        Component copiedComponent = this.gameObject.AddComponent(componentType);
+
+        // Iterate through each field and copy its value
+        foreach (var field in componentFields)
+        {
+            // Get the value of the field from the original component
+            object value = field.GetValue(component);
+
+            // Set the value of the field in the added component
+            field.SetValue(copiedComponent, value);
+        }
+
+        return copiedComponent;
     }
 }
